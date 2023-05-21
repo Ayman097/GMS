18a108c1b2c81206269b417e543c9926e3907309
Ayman097 <engaymanfarag1997@gmail.com>
2023-05-21 18:51:57 +0300

    Adding Home page and config postgres DB

--- GymManagementSystem/GymManagementSystem/settings.py
+++ GymManagementSystem/GymManagementSystem/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'GymManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gym',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'port': '5432'
     }
 }
 
